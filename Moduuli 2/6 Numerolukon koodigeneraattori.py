a0633aee611c146af1902f2bf055f8ff25790725
Mimo <anna.serenius@metropolia-fi>
2025-08-19 18:38:58 +0300

    Numerolukon koodigeneraattori valmis

---
+++ Moduuli 2/6 Numerolukon koodigeneraattori.py
@@ -0,0 +1,13 @@
+import random
+
+n1 = random.randint(0,9)
+n2 = random.randint(0,9)
+n3 = random.randint(0,9)
+
+n4 = random.randint(1,6)
+n5 = random.randint(1,6)
+n6 = random.randint(1,6)
+n7 = random.randint(1,6)
+
+print("Numerolukon kolminumeroinen koodi: ", n1, n2, n3)
+print("Numerolukon nelinumeroinen koodi: ", n4, n5, n6, n7)
