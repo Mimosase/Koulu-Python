40032df2b6558149bb10bc0808807072e933f1fb
Mimo <anna.serenius@metropolia-fi>
2025-08-19 10:45:33 +0300

    Keskiajan painolaskin tehtävä aloitettu

--- Moduuli 2/Keskiajan painolaskin.py
+++ Moduuli 2/Keskiajan painolaskin.py
@@ -1,10 +1,12 @@
 print("Hei keskiajalle jäänyt kaveri! Muunnetaan antiikkiset mittasi nykyäjan mittoihin.")
 print("Syötä massa leivisköinä, nauloina ja luoteina.")
 
-leiviskä = int(input("Leiviskät:"))
-naula = int(input("Naulat:"))
-luoti = int(input("Luodit:"))
+amount_le = int(input("Leiviskät:"))
+amount_na = int(input("Naulat:"))
+amount_lu = float(input("Luodit:"))
 
-# Yksi leiviskä on 20 naulaa.
-# Yksi naula on 32 luotia.
-# Yksi luoti on 13,3 grammaa.
+luoti = 13,3
+naula = 32 * luoti
+leiviskä = 20 * naula
+
+print((luoti*amount_lu) + (naula * amount_na) + (leiviskä*amount_le))
