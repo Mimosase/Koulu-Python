2bdbe17cfcb111f394c5d71809732b85f043f69d
Mimo <anna.serenius@metropolia-fi>
2025-08-19 09:57:30 +0300

    Kolme kokonaislukua- tehtävä tehty

---
+++ Moduuli 2/Kolme kokonaislukua.py
@@ -0,0 +1,8 @@
+print("Syötä kolme kokonaislukua!")
+n1 = int(input("Luku 1:"))
+n2 = int(input("Luku 2:"))
+n3 = int(input("Luku 3:"))
+summa = n1+n2+n3
+print("Summa =", (summa))
+print("Tulo =", (n1*n2*n3))
+print("Keskiarvo =", (summa/3))
