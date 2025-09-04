eff20f767ab75c8755a8985775c904f5293cf2b4
Mimo <anna.serenius@metropolia-fi>
2025-09-04 14:50:41 +0300

    päivityksiä

---
+++ Moduuli 6/1_Nopanheitto.py
@@ -0,0 +1,5 @@
+# Kirjoita funktio nimeltä roll_dice, joka palauttaa satunnaisen nopan silmäluvun väliltä 1..6.
+# Funktiolla ei ole parametreja. Kirjoita pääohjelma, joka heittää noppaa, kunnes tulee 6.
+# Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
+
+import random
