cbff340c147ec4447a0d57efe3d6ecfbdc72e09e
Mimosa Serenius <anna.serenius@metropolia.fi>
2025-10-29 00:28:35 +0200

    päivityksiä

---
+++ Moduuli 10/Tehtävä 3.py
@@ -0,0 +1,42 @@
+class Elevator:
+
+    def __init__(self, bottom_floor, top_floor):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.current_floor = bottom_floor
+
+    def floor_up(self):
+        self.current_floor += 1
+
+    def floor_down(self):
+        self.current_floor -= 1
+
+    def go_to_floor(self,request):
+        if request > self.current_floor:
+            while self.current_floor != request:
+                self.floor_up()
+
+        if request < self.current_floor:
+            while self.current_floor != request:
+                self.floor_down()
+
+        return
+
+class Building:
+
+    def __init__(self, bottom_floor, top_floor, number_of_elevators):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.number_of_elevators = number_of_elevators
+        self.elevators = []
+
+        for kpl in range(number_of_elevators):
+            building_elevator = Elevator(bottom_floor,top_floor)
+            self.elevators.append(building_elevator)
+
+    def run_elevator(self, elevator_number, request):
+        self.elevators[elevator_number].go_to_floor(request)
+
+    def fire_alarm(self):
+        for index in range(self.number_of_elevators):
+            self.run_elevator(self.bottom_floor)
---
+++ Moduuli 10/Tehtävä 4.py
@@ -0,0 +1,61 @@
+import random
+
+class Car:
+
+    def __init__(self, license_plate, maximum_speed=0, current_speed=0, travelled_distance=0):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = current_speed
+        self.travelled_distance = travelled_distance
+
+    def accelerate(self, change):
+        self.current_speed += change
+        if self.current_speed < 0:
+            self.current_speed = 0
+        elif self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        return
+
+    def drive(self, hours):
+        self.travelled_distance += self.current_speed * hours
+        return
+
+class Race:
+    def __init__(self, name, distance, cars):
+        self.name = name
+        self.distance = distance
+        self.cars = cars
+
+    def hour_passes(self):
+        #hours += 1
+        for car in self.cars:
+            car.accelerate(random.randint(-10, 15))
+            car.drive(1)
+
+    def print_status(self):
+        print(self.cars)
+
+    def race_finished(self):
+
+
+
+def race():
+    cars = [Car("ABC-1", random.randint(100, 200)), Car("ABC-2", random.randint(100, 200)),
+        Car("ABC-3", random.randint(100, 200)), Car("ABC-4", random.randint(100, 200)),
+        Car("ABC-5", random.randint(100, 200)), Car("ABC-6", random.randint(100, 200)),
+        Car("ABC-7", random.randint(100, 200)), Car("ABC-8", random.randint(100, 200)),
+        Car("ABC-9", random.randint(100, 200)), Car("ABC-10", random.randint(100, 200))]
+
+    goal = False
+    hours = 0
+
+    while goal != True:
+        hours += 1
+        for car in cars:
+            car.accelerate(random.randint(-10, 15))
+            car.drive(1)
+            if car.travelled_distance >= 10000:
+                goal = True
+
+    cars.sort(key = lambda x: x.travelled_distance, reverse=True)
+    return cars
